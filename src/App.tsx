bd2e63756b09d53f50fd129efa769e6921e53f17
eplq <91148468+eplq@users.noreply.github.com>
2022-09-26 11:23:03 +0200

    height typo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className="w-screen bg-gradient-to-br from-blue-800 to-yellow-800 flex justify-center">
             <main className="min-h-screen w-3/4 md:w-1/2 lg:w-1/3 flex flex-col justify-evenly items-center">
                 <textarea
-                    className="bg-transparent border-none outline-none w-10/12 text-3xl h-1/ text-neutral-200 placeholder:text-center placeholder:text-neutral-500"
+                    className="bg-transparent border-none outline-none w-10/12 text-3xl h-1/2 text-neutral-200 placeholder:text-center placeholder:text-neutral-500"
                     defaultValue={text}
                     placeholder="Amor a Roma"
                     autoFocus
